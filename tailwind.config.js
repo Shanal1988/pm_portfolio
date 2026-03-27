8a72f829959cc8e0ccd6a7c62001bc57d19a14ec
Shanal1988 <32175720+Shanal1988@users.noreply.github.com>
2026-03-27 16:20:20 +0000

    Add custom Tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'navy': {
+          800: '#1e3a5f',
+          900: '#0f172a',
+        },
+      },
+    },
   },
   plugins: [],
 };
